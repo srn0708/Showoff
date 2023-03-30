e2cfa4b8f13d66f4f4013e06c0d261686ed08348
茹楠 宋 <songrunan@outlook.com>
2023-03-30 12:41:05 +0800

    creaed Class1

---
+++ TempGitShowoff/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempGitShowoff
+{
+    internal static class Class1
+    {
+        public static void Print(this string text) { Console.WriteLine(text); }
+    }
+}
